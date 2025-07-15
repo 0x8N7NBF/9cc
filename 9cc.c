4615f0750793a51777b707e3a5d5b25828488d18
0x8N7NBF <218385633+0x8N7NBF@users.noreply.github.com>
2025-07-15 21:46:05 +0900

    四則演算の実装に向けた関数の準備
    四則演算をBNFで記述
    再帰下降構文解析でASTを生成
    スタックマシンとしてasmコードを出力

--- 9cc.c
+++ 9cc.c
@@ -123,6 +123,125 @@ Token *tokenize() {
     return head.next;
 }
 
+// ASTのノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// ASTのノードの型
+struct Node {
+    NodeKind kind; // ノードの型
+    Node *lhs; // left hand side
+    Node *rhs; // right hand side
+    int val; // 型がND_NUMの場合, その数値
+};
+
+// 非終端ノード（2項演算子）の作成
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+// 終端ノード（数値）の作成
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+// プロトタイプ宣言
+Node *expr();
+Node *mul();
+Node *primary();
+
+// expr = mul ( "+" mul | "-" mul )*
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+// mul = primary ( "*" primary | "/" primary )*
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+// primary = "(" expr ")" | num
+Node *primary() {
+    // 次のトークンが"("なら, "(" expr ")"のはず
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    // そうでなければ数値のはず
+    return new_node_num(expect_number());
+}
+
+// ASTからasmコードを生成
+void gen(Node *node) {
+    // 終端ノード (数値)ならスタックにpush
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    // 非終端ノード (2項演算子)なら, まず左辺と右辺をそれぞれ処理
+    gen(node->lhs);
+    gen(node->rhs);
+
+    // スタックから2つの値をpopして、それらの計算結果をpush
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            // x86-64でのidivの仕様のため. 
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "引数の個数が正しくありません\n");
